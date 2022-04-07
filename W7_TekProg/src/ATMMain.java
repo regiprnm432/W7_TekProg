a5684e5e0b50fbbcc48de3a7d5f222ed12b72dcf
regipurnama432 <regiprnm432@gmail.com>
2022-04-07 20:35:00 +0700

    W7_TekProg

---
+++ W7_TekProg/src/ATMMain.java
@@ -0,0 +1,12 @@
+
+public class ATMMain {
+   // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+   }
+} 
+
+
+
+ 
